5523e8ae1698b41a1ea066e6d569cb77592d08dd
nitz88 <1853404+nitz88@users.noreply.github.com>
2022-10-02 23:59:14 +0200

    added unit test cases for add and search functionality

--- src/app/products/product-search/product-search.component.spec.ts
+++ src/app/products/product-search/product-search.component.spec.ts
@@ -1,26 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MockProvider } from 'ng-mocks';
 import { ProductHttpService } from '../product-http.service';
 import { ProductSearchComponent } from './product-search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe(ProductSearchComponent.name, () => {
   let component: ProductSearchComponent;
   let fixture: ComponentFixture<ProductSearchComponent>;
+  let service: ProductHttpService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+      ],
       declarations: [ProductSearchComponent],
-      providers: [MockProvider(ProductHttpService)],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductSearchComponent);
+    service = TestBed.inject(ProductHttpService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -28,4 +37,18 @@ describe(ProductSearchComponent.name, () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call searchMethod when adding search term', fakeAsync(() => {
+    const searchMethodSpy = jest.spyOn(service, 'search').mockReturnValue(of());
+    component.search.setValue('phone');
+    tick(2500);
+    expect(searchMethodSpy).toBeCalled();
+  }));
+
+  it('should call getAll method when we remove search term', fakeAsync(() => {
+    const getAllMethodSpy = jest.spyOn(service, 'getAll').mockReturnValue(of());
+    component.search.setValue('');
+    tick(2000);
+    expect(getAllMethodSpy).toBeCalled();
+  }));
 });
--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent, MockProvider } from 'ng-mocks';
 import { ProductHttpService } from './product-http.service';
@@ -15,6 +16,8 @@ import { ProductsComponent } from './products.component';
 describe(ProductsComponent.name, () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let service: ProductHttpService;
+  let matBottomSheet: MatBottomSheet;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,7 +29,8 @@ describe(ProductsComponent.name, () => {
         MatFormFieldModule,
         MatInputModule,
         MatTooltipModule,
-        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        CommonModule
       ],
       declarations: [ProductsComponent, MockComponent(ProductSearchComponent)],
       providers: [MockProvider(ProductHttpService)],
@@ -36,10 +40,28 @@ describe(ProductsComponent.name, () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(ProductHttpService);
+    matBottomSheet = TestBed.inject(MatBottomSheet);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(service).toBeTruthy();
+    expect(matBottomSheet).toBeTruthy();
+  });
+
+  it('should call addProduct method when button click happens', () => {
+    let button = fixture.debugElement.nativeElement.querySelector('#addButton');
+    let addButtonEvent = jest.spyOn(component, 'addProduct');
+    button.click();
+    expect(addButtonEvent).toBeCalled();
+  });
+
+  it('should open matBottomSheet when addButton clicked', ()=> {
+    let button = fixture.debugElement.nativeElement.querySelector('#addButton');
+    let matBottomSheetSpy = jest.spyOn(matBottomSheet, 'open');
+    button.click();
+    expect(matBottomSheetSpy).toBeCalled();
   });
 });
